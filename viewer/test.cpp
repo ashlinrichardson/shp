ae74244bf20555b06677da0e71fed5e9949a4f72
ashlinrichardson <richardson.ashlin@gmail.com>
2017-11-02 13:58:12 -0700

    tidy
    tidy

--- viewer/test.cpp
+++ viewer/test.cpp
@@ -14,7 +14,7 @@ long int getFileSize(std::string fn){
   i.open(fn.c_str(), ios::binary);
   if(!(i.is_open())){
     printf("Error: was unable to open file: %s\n", (fn.c_str()));
-    return (-1);
+    return -1;
   }
   i.seekg(0, ios::end);
   long int len = i.tellg();
@@ -25,12 +25,13 @@ int main(int argc, char ** argv){
   std::string fn("TA_PEP_SVW_polygon.json");
   long int fs = getFileSize(fn);
   char * fd = (char *)(void *)malloc(fs);
-  memset(fd, '\0',fs);
+  memset(fd, '\0', fs);
   printf("load file\n");
   FILE * f = fopen(fn.c_str(), "rb");
   long int br = fread(fd, 1, fs, f);
   if(br != fs){
-    printf("Error: br (%ld)!= fs (%ld)\n", (long int)br, (long int)fs); exit(1);
+    printf("Error: br (%ld)!= fs (%ld)\n", (long int)br, (long int)fs);
+    exit(1);
   }
   long int i;
   long int f2 = fs;
@@ -43,7 +44,7 @@ int main(int argc, char ** argv){
   cout << f2 << endl;
 
   char * fd2 = (char *)(void *)malloc(f2);
-  memset(fd2, '\0',f2);
+  memset(fd2, '\0', f2);
 
   printf("remove newline\n");
   long int ci = 0;
@@ -55,7 +56,7 @@ int main(int argc, char ** argv){
 
   printf("parsing json...\n");
   Document document;
-  document.Parse((const char *)(void *) fd2);
+  document.Parse((const char *)(void *)fd2);
   assert(document.IsObject());
 
   static const char* kTypeNames[] = {
@@ -63,17 +64,12 @@ int main(int argc, char ** argv){
   };
 
   for(Value::ConstMemberIterator itr = document.MemberBegin(); itr != document.MemberEnd(); ++itr){
-    printf("%sType of member %s%s%s is %s%s%s\n",
-    KGRN,
-    KRED,
-    itr->name.GetString(),
-    KGRN,
-    KYEL,
-    kTypeNames[itr->value.GetType()],
-    KGRN
-    );
+    printf("%sType of member %s%s%s: %s%s%s\n", KGRN, KRED, itr->name.GetString(),
+    KGRN, KYEL, kTypeNames[itr->value.GetType()], KGRN);
+
     printf("here\n");
-    if(!strncmp("Array\0", kTypeNames[itr->value.GetType()],5)){
+    if(!strncmp("Array\0", kTypeNames[itr->value.GetType()], 5)){
+
       //assert(itr->IsArray());
       printf("\tisArray=true\n");
       const Value& a = itr->value;
@@ -84,34 +80,23 @@ int main(int argc, char ** argv){
 
         // for all the members in iter2
         for (Value::ConstMemberIterator itr3 = itr2->MemberBegin(); itr3 != itr2->MemberEnd(); ++itr3){
-          printf("\t%sType of member %s%s%s is %s%s%s\n",
-                 KGRN,
-                 KRED,
-                 itr3->name.GetString(), //iter3 is reference to a member
-                 KGRN,
-                 KYEL,
-                 kTypeNames[itr3->value.GetType()],
-                 KGRN
-          );
-      
-          if(!strncmp("String\0", kTypeNames[itr3->value.GetType()],5)){
+          printf("\t%sType of member %s%s%s: %s%s%s\n", KGRN, KRED, itr3->name.GetString(),
+          KGRN, KYEL, kTypeNames[itr3->value.GetType()], KGRN);
+
+          if(!strncmp("String\0", kTypeNames[itr3->value.GetType()], 5)){
             printf("\t\t\t%s\n", itr3->value.GetString());
           }
-    
-          if(!strncmp("Object\0", kTypeNames[itr3->value.GetType()],5)){
+
+          if(!strncmp("Object\0", kTypeNames[itr3->value.GetType()], 5)){
             if(!strncmp("geometry", itr3->name.GetString(), 8)){
               printf("\t\t%s\n", itr3->name.GetString());
 
-              
-              
-            };
-
-
+            }
           }
         }
       }
     }
-}
-    printf("done\n");
-    return 0;
   }
+  printf("done\n");
+  return 0;
+}
